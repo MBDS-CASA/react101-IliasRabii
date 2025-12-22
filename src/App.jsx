d03ef9b6674e04f422ddac4883235e28e0af1cb5
IliasRabii <ilias2003rabii@gmail.com>
2025-12-22 15:23:42 +0100

    TD01 : Ajouter un composant Header dans la page

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,11 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
 
+
+function Header() {
   return (
-    <>
-      <div>
+     <div>
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
@@ -16,8 +15,14 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React ilias</h1>
-      <div className="card">
+  )
+}
+
+function Content() {
+    const [count, setCount] = useState(0)
+
+  return (
+    <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
@@ -25,6 +30,20 @@ function App() {
           Edit <code>src/App.jsx</code> and save to test HMR
         </p>
       </div>
+  )
+}
+function App() {
+  const [count, setCount] = useState(0)
+
+
+  return (
+    <>
+      <Header />
+       <Header />
+        <Header />
+<h1> ilias themost famous one in any contry</h1>
+<Content />
+ 
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
---
+++ src/compenents/Header.jsx
@@ -0,0 +1,15 @@
+function Header() {
+  return (
+    <header style={{ textAlign: "center", padding: "20px" }}>
+      <img
+        src="https://reactjs.org/logo-og.png"
+        alt="Logo formation"
+        width="100"
+      />
+      <h1>Introduction à React</h1>
+      <h2>À la découverte des premières notions de React</h2>
+    </header>
+  );
+}
+
+export default Header;
