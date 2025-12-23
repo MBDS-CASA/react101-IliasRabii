301a63e7e24abd6210fb58a5747b502cb035982f
IliasRabii <ilias2003rabii@gmail.com>
2025-12-23 01:14:51 +0100

    TD06 : Ajout recherche, tri et pagination

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,9 @@ import Header from "./compenents/Header";
 import Footer from "./compenents/Footer";
 import Menu from "./compenents/Menu";
 import Notes from "./compenents/Notes";
-import Students from "./compenents/Students"; // Nouvel import
-import Subjects from "./compenents/Subjects"; // Nouvel import
-import About from "./compenents/About";       // Nouvel import
+import Students from "./compenents/Students";
+import Subjects from "./compenents/Subjects";
+import About from "./compenents/About";
 import "./App.css";
 
 function App() {
@@ -30,9 +30,18 @@ function App() {
     <div className="app">
       <Menu activePage={activePage} setActivePage={setActivePage} />
       <Header />
-      <div style={{ width: "90%", maxWidth: "800px", margin: "0 auto 100px" }}>
+      
+      {/* L'ajout de key={activePage} force React à détruire et recréer ce bloc 
+         à chaque changement de page, ce qui déclenche l'animation "fade-in".
+      */}
+      <div 
+        key={activePage} 
+        className="fade-in" 
+        style={{ width: "90%", maxWidth: "800px", margin: "0 auto 100px" }}
+      >
          {renderContent()}
       </div>
+
       <Footer />
     </div>
   );
